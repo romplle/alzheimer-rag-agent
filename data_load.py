dee3bfad73e297ca70da7c61c9a604d432be9db6
Kirill Serikov <glugol100@gmail.com>
2026-01-26 21:06:10 +0300

    improve text extraction with pymupdf.layout and paragraph merging

--- data_load.py
+++ data_load.py
@@ -1,3 +1,4 @@
+import pymupdf.layout
 import pymupdf4llm
 from pathlib import Path
 
@@ -9,13 +10,41 @@ Path(OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
 pdf_files = list(Path(INPUT_FOLDER).glob("*.pdf"))
 print(f"Найдено {len(pdf_files)} pdf-файлов.")
 
+def merge_broken_paragraphs(md: str) -> str:
+    lines = md.splitlines()
+    merged = []
+    
+    for line in lines:
+        line = line.strip()
+        if not line:
+            merged.append("")
+            continue
+        
+        if (
+            merged
+            and merged[-2]
+            and not merged[-2].endswith(('.', '!', '?', ':'))
+            and line[0].islower()
+        ):
+            merged[-2] += " " + line
+        else:
+            merged.append(line)
+    
+    return "\n".join(merged)
+
 for pdf_path in pdf_files:
     md_text = pymupdf4llm.to_markdown(
         pdf_path,
         pages=None,
-        hdr_info=True,
+        hdr_info=False,
         write_images=False,
+        footer=False,
+        header=False,
+        margins=50,
+        use_ocr=False
     )
+
+    md_text = merge_broken_paragraphs(md_text)
     
     output_path = Path(OUTPUT_FOLDER) / f"{pdf_path.stem}.md"
     with open(output_path, "w", encoding="utf-8") as f:
