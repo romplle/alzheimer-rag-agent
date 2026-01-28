232c15fbb6c56c5a7d78951ecde8a3e749f02e57
Kirill Serikov <glugol100@gmail.com>
2026-01-28 07:01:54 +0300

    add chromadb

--- rag.py
+++ rag.py
@@ -1,5 +1,5 @@
-import os
 import streamlit as st
+import chromadb
 
 from llama_index.core import (
     Settings,
@@ -12,6 +12,7 @@ from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.postprocessor import SentenceTransformerRerank
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.openrouter import OpenRouter
+from llama_index.vector_stores.chroma import ChromaVectorStore
 
 from api_key import OPENROUTER_API_KEY
 from prompt import QA_PROMPT
@@ -31,17 +32,25 @@ def load_query_engine():
 
     Settings.llm = OpenRouter(
         api_key=OPENROUTER_API_KEY,
-        model='mistralai/devstral-2512:free',
+        model='arcee-ai/trinity-large-preview:free',
         max_tokens=2000
     )
 
-    if not os.path.exists(PERSIST_DIR):
-        documents = SimpleDirectoryReader(input_dir=DATA_FOLDER, required_exts='.md').load_data(show_progress=True)
+    chroma_client = chromadb.PersistentClient(
+        path=PERSIST_DIR,
+        settings=chromadb.Settings(anonymized_telemetry=False)
+    )
+    chroma_collection = chroma_client.get_or_create_collection('alzheimer_papers')
+    
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR, vector_store=vector_store)
+
+    if chroma_collection.count() == 0:
+        documents = SimpleDirectoryReader(input_dir=DATA_FOLDER,required_exts=['.md']).load_data(show_progress=True)
         splitter = SentenceSplitter(chunk_size=1000, chunk_overlap=100)
-        index = VectorStoreIndex.from_documents(documents, transformations=[splitter], show_progress=True)
+        index = VectorStoreIndex.from_documents(documents, transformations=[splitter], storage_context=storage_context, show_progress=True)
         index.storage_context.persist(persist_dir=PERSIST_DIR)
     else:
-        storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
         index = load_index_from_storage(storage_context)
 
     reranker = SentenceTransformerRerank(
@@ -57,3 +66,6 @@ def load_query_engine():
         text_qa_template=QA_PROMPT,
     )
     return query_engine
+
+if __name__ == '__main__':
+    load_query_engine()
